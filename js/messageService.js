3d258cba1f264411b811a7b1e719f3c8efb02f84
mkrajews <mimikrajewski@Jakes-MacBook-Pro-2.local>
2016-10-31 19:39:41 -0600

    cookie chat updated

--- js/messageService.js
+++ js/messageService.js
@@ -11,10 +11,11 @@ this.getMessages = function() {
   });
 }
 
-  //On the line below create the postMessage method. This method will add data to the backend server.
-  //The url for the request needs to be 'http://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
-  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+//On the line below create the postMessage method. This method will add data to the backend server.
+//The url for the request needs to be 'http://practiceapi.devmounta.in/api/chats'
+//Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
+//Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+
   this.postMessage = function(yourMessage) {
     return $http({
   url: 'http://practiceapi.devmounta.in/api/chats',
@@ -22,6 +23,5 @@ this.getMessages = function() {
   data: {message: yourMessage}
 });
 };
-
 
 });
